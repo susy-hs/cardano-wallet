b8fb3e87094655e08bc8a95b4f5de932217eae5e
Johannes Lund <johannes.lund@iohk.io>
2019-05-09 16:50:55 +0200

    Factor out unsafeLookupEnv
    - Added System.Environment.Lookup and System.Environment.LookupSpec to
    core
    
    - Removed common parts from
        - Cardano.Environment.HttpBridge
        - Cardano.Environment.HttpBridgeSpec
        - Cardano.Environment.Jormungandr

--- lib/http-bridge/test/unit/Cardano/Environment/HttpBridgeSpec.hs
+++ lib/http-bridge/test/unit/Cardano/Environment/HttpBridgeSpec.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -8,17 +7,11 @@ module Cardano.Environment.HttpBridgeSpec where
 import Prelude
 
 import Cardano.Environment.HttpBridge
-    ( ErrMissingOrInvalidEnvVar (..), Network, unsafeLookupEnv )
-import Data.Maybe
-    ( isNothing )
+    ( Network )
 import Data.Proxy
     ( Proxy (..) )
-import Data.Text.Class
-    ( TextDecodingError (..) )
-import System.Environment
-    ( setEnv, unsetEnv )
 import Test.Hspec
-    ( Spec, describe, it, shouldThrow )
+    ( Spec, describe )
 import Test.QuickCheck
     ( Arbitrary (..) )
 import Test.QuickCheck.Arbitrary.Generic
@@ -30,46 +23,6 @@ spec :: Spec
 spec = do
     describe "Can perform roundtrip textual encoding & decoding" $ do
         textRoundtrip $ Proxy @Network
-
-    describe "ErrMissingOrInvalidEnvVar (Show / displayException)" $ do
-        let errNoAdditionalContext = ErrMissingOrInvalidEnvVar
-                { name = "PATATE"
-                , command = "my-command"
-                , additionalContext = Nothing
-                }
-        let errWithAdditionalContext = ErrMissingOrInvalidEnvVar
-                { name = "PATATE"
-                , command = "my-command"
-                , additionalContext = Just
-                    ("💩"
-                    , TextDecodingError
-                        { getTextDecodingError = "not a valid value" }
-                    )
-                }
-        it (show errNoAdditionalContext) True
-        it (show errWithAdditionalContext) True
-
-    describe "unsafeLookupEnv" $ do
-        it "throws with no context when variable isn't present" $ do
-            unsetEnv "PATATE" -- Just in case
-            let io =
-                    unsafeLookupEnv @Network "PATATE" `seq` (return ())
-            let selector (ErrMissingOrInvalidEnvVar n _ c) =
-                    n == "PATATE" && isNothing c
-            io `shouldThrow` selector
-
-        it "throws with extra context when variable is present but invalid" $ do
-            setEnv "PATATE" "not-a-network"
-            let ctx =
-                    ( "not-a-network"
-                    , TextDecodingError "not-a-network is neither \"mainnet\",\
-                        \ \"testnet\" nor \"staging\"."
-                    )
-            let selector (ErrMissingOrInvalidEnvVar n _ c) =
-                    n == "PATATE" && c == Just ctx
-            let io =
-                    unsafeLookupEnv @Network "PATATE" `seq` (return ())
-            io `shouldThrow` selector
 
 {-------------------------------------------------------------------------------
                                Arbitrary Instances
